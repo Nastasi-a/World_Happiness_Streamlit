012af7ec9f37f6b267b7406285bc62e829143054
Nastasi-a <138889463+Nastasi-a@users.noreply.github.com>
2024-04-11 12:22:21 +0200

    1

--- world_happiness.py
+++ world_happiness.py
@@ -39,7 +39,7 @@ if page == pages[1] :
   st.subheader('Columns')
 
   table_data = {
-    "📊":  ["🌍", "🌐", "📅", "⭐", "💰", "🤝", "🏥", "🔓", "🎁", "🔍", "😊", "😞"]
+    "📊": ["🌍", "🌐", "📅", "⭐", "💰", "🤝", "🏥", "🔓", "🎁", "🔍", "😊", "😞"],
     "Column": ["Country name", "Regional indicator", "Year", "Ladder score", "Logged GDP per capita",
                "Social support", "Healthy life expectancy", "Freedom to make life choices", "Generosity",
                "Perceptions of corruption", "Positive affect", "Negative affect"],
